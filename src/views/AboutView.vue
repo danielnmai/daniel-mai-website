ecf54e00bc38772fca33653a77a5f8db78bfe467
Daniel Mai <danielmai1728@gmail.com>
2023-12-15 15:18:34 -0800

    change bio texts

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,16 +2,19 @@
   <main class="min-h-full">
     <h1 class="text-[2rem] font-bold">About Me</h1>
     <p class="mt-5">
-      Hi! I am Daniel Mai, a senior software engineer based in California. I
-      have built more than 15 software projects for Infuse, a software
-      development agency from 2018 to 2023. Those projects' deadlines are as
-      short as 3 months and as long as 2 years.
+      Hi! I am Daniel Mai, a software engineer based in California, US. I write
+      code to make the world a little better. I love crafting new things,
+      playing with new technologies and being in a team with great people.
     </p>
     <p class="mt-5">
-      I enjoy building websites and mobile apps using JavaScript. I also like
-      doing system and architecture design, and am familar with full software
-      development cycle, from frontend & backend development to CI/CD, devops
-      and cloud deployment.
+      I am a JavaScript geek. I'm familar with a full cycle software
+      development, from frontend with React, backend with NodeJS, and deployment
+      with AWS.
+    </p>
+    <p class="mt-5">
+      In my free time, I am an avid PC gamer and enjoy reading books and working
+      out. A one-time IronMan finisher, I am also a triathlete and spend time
+      doing swimming, cycling and running on a daily basis.
     </p>
     <p class="mt-5">
       I graduated from Northwestern Polytechnic University with a Bachelor
